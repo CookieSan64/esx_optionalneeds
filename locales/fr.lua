e60c8890329c1b249424d1ccd73b05a9712f5fb9
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 02:16:04 +0100

    Initial commit

---
+++ locales/fr.lua
@@ -0,0 +1,9 @@
+Locales['fr'] = {
+	
+	['used_beer'] = 'vous avez utilisé 1x Bière',
+	['used_champagne'] = 'vous avez utilisé 1x Champagne',
+	['used_cocaine'] = 'vous avez utilisé 1x Cocaïne',
+	['used_weed'] = 'vous avez utilisé 1x Weed',
+	['used_meth'] = 'vous avez utilisé 1x Meth',
+	['used_opium'] = 'vous avez utilisé 1x Opium',
+}
